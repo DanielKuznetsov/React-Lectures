1af18c751b340fe7c7d7e0e9d8aa007acf410411
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-11-01 23:36:45 -0700

    Enable name to show up if checkbox is clicked

--- Chapter 1/chapter-1/src/MyName.js
+++ Chapter 1/chapter-1/src/MyName.js
@@ -2,16 +2,20 @@ import React, {useState} from "react";
 
 function MyName() {
     const [name, setName] = useState("Robert");
+    const [enableFirstName, setEnableFirstName] = useState(false);
 
     function handleEventChange(evt) {
         setName(evt.target.value);
-        console.log(evt.target)
+    }
+
+    function handleEnableChange(evt) {
+        setEnableFirstName(!enableFirstName);
     }
 
     return <>
         <div>
-            <h1>My name is: {name}</h1>
-            
+            <h1>My name is: {enableFirstName ? name : ""}</h1>
+            <input type="checkbox" value={enableFirstName} onChange={handleEnableChange} />
             {/* Event function is passed with a zero parameter and is not called */}
             {/* React creates its own SyntheticEvent wrapper over original event */}
             {/* and passes it to your event handler as first argument */}
