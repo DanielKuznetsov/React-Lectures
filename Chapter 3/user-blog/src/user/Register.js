fcd068299047c2ddf0603ba7fc15b01dd249cfe3
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-11-04 17:06:48 -0700

    Made Login and Register components dynamic

--- Chapter 3/user-blog/src/user/Register.js
+++ Chapter 3/user-blog/src/user/Register.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function Register() {
+function Register({setUser}) {
+    const [username, setUsername] = useState("");
+    const [ password, setPassword ] = useState('');
+    const [ passwordRepeat, setPasswordRepeat ] = useState('');
+
+    function handleUsername(evt) {
+        setUsername(evt.target.value);
+    }
+
+    function handlePassword (evt) {
+        setPassword(evt.target.value)
+    }
+
+    function handlePasswordRepeat (evt) {
+        setPasswordRepeat(evt.target.value)
+    }
+
     return (
-        <form onSubmit={e => e.preventDefault()}>
+        <form onSubmit={e => {e.preventDefault(); setUser(username)}}>
             <label htmlFor="register-username">Username:</label>
-            <input type="text" name="register-username" id="register-username"/>
+            <input value={username} onChange={handleUsername} type="text" name="register-username" id="register-username"/>
             <label htmlFor="register-password">Password:</label>
-            <input type="password" name="register-password" id="register-password"/>
+            <input type="password" value={password} onChange={handlePassword} name="register-password" id="register-password"/>
             <label htmlFor="register-password-repeat">Repeat password:</label>
-            <input type="password" name="register-password-repeat" id="register-password-repeat"/>
-            <input type="submit" value="Register"/>
+            <input type="password" value={passwordRepeat} onChange={handlePasswordRepeat} name="register-password-repeat" id="register-password-repeat"/>
+            <input type="submit" value="Register" disabled={username.length === 0 || password.length === 0 || password !== passwordRepeat}/>
         </form>
     )
 }
